8783600a5f623cd97f5bb9d1b7e0240bb4d5b61e
corn910053 <corn910053@users.noreply.github.com>
2015-03-15 16:48:11 +0800

    Create random.java
    Create a class for the random number generation

---
+++ random.java
@@ -0,0 +1,23 @@
+public class random{
+	public static void main(String[] args) {
+		int randomNumber = 1 + (int)(Math.random()*50);
+		int randomNumberByMode = randomNumber % 4;
+
+		switch(randomNumberByMode){
+			case 0:
+					System.out.println("Zero: pineapple ,and your random number is: " + randomNumber);
+			break;
+			case 1:
+					System.out.println("One: apple ,and your random number is: " + randomNumber);
+			break;
+			case 2:
+					System.out.println("Two: watermelon ,and your random number is: " + randomNumber);
+			break;
+			case 3:
+					System.out.println("Three: peach ,and your random number is: " + randomNumber);
+			break;
+			default:
+					System.out.println("Error ,and your random number is: " + randomNumber);			
+		}
+	}
+}
